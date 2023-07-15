a7d994e7e5cb06849d04f2de9e93aa9dc40dc8cd
Vishal <vishaljain7100@gmail.com>
2023-07-16 00:50:21 +0530

    Page is ready but backimage is in process

--- Men's Wear/menswear.js
+++ Men's Wear/menswear.js
@@ -1,24 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+  localStorage.clear()
+})
 let section = document.getElementById('section')
 let sectionDiv = section.querySelectorAll('div')
 Array.from(sectionDiv).forEach((div) => {
-    div.addEventListener('click', (event) => {
-      var selectedDiv = event.target;
-      var DivBgImageSRC = window.getComputedStyle(selectedDiv).getPropertyValue('background-image');
-      let imageurl = DivBgImageSRC
+  div.addEventListener('click', (event) => {
+    
+    var selectedDiv = event.target;
+    var DivBgImageSRC = window.getComputedStyle(selectedDiv).getPropertyValue('background-image');
+    localStorage.setItem('postRequestBody', DivBgImageSRC);
 
-      fetch('Product.html', {
-        method: 'POST',
-        body: imageurl,
-      })
-        .then(function (response) {
-          return response.json(); // Parse response as JSON
-        })
-        .then(function (data) {
-          console.log(data); // Access imageurl value
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    });
+    setInterval(() => {
+      for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        if (key.startsWith("postRequestBody")) {
+          window.location.href = "Product.html"
+        }
+      }
+    },1000)
   });
-  
+});
+
--- common javascript/common.js
+++ common javascript/common.js
@@ -1,7 +1,21 @@
-fetch("MensWear.html")
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+var postRequestBody = localStorage.getItem('postRequestBody');
+
+if (postRequestBody) {
+    let mainPhoto = document.getElementById("mainPhotodiv")
+
+    var ChoiceOne = document.getElementsByClassName('Choice-one');
+    for (var i = 0; i < ChoiceOne.length; i++) {
+        var divElement = ChoiceOne[i];
+        divElement.style.backgroundImage = postRequestBody;
+    }
+
+    var ChoiceTwo = document.getElementsByClassName('Choice-two');
+    for (var i = 0; i < ChoiceTwo.length; i++) {
+        var divElement = ChoiceTwo[i];
+        divElement.style.backgroundImage = postRequestBody;
+    }
+
+    mainPhoto.style.backgroundImage = postRequestBody;
+} else {
+    console.log('Background image URL not found in local storage');
+}
