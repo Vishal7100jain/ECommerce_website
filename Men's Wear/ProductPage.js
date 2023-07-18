9dc1b4490e30b18a559ffd7f636602ef629fac0f
Vishal <vishaljain7100@gmail.com>
2023-07-19 00:28:46 +0530

    Photo changing is ready onclick and size div bg color changing is also ready , now working on navbar

--- Men's Wear/ProductPage.js
+++ Men's Wear/ProductPage.js
@@ -8,15 +8,44 @@ let WatchBackimage = "url('https://rukminim2.flixcart.com/image/416/416/xif0q/sm
 // Selecting all image div to change image based on image clicked
 let mainPhoto = document.getElementById('mainPhotodiv')
 
-// Choice one image
-let ChoiceOne = document.getElementsByClassName('.Choice-one')
+//****************************     Choice one image    *****************************\\
+
+//Funtion to change main image to image One
+let ChoiceOne = document.getElementsByClassName('Choice-one')
 for (i = 0; i < ChoiceOne.length; i++) {
     let imageOne = ChoiceOne[i]
+    let imageOneUrl = window.getComputedStyle(imageOne).getPropertyValue('background-image')
+
     imageOne.addEventListener('click', () => {
-        let imageOneUrl = window.getComputedStyle(ChoiceOne).getPropertyValue("background-image")
-        console.log(imageOneUrl)
+        mainPhoto.style.backgroundImage = imageOneUrl
     })
 }
-let ChoiceTwo = document.getElementsByClassName('.Choice-two')
 
- 
+//Funtion to change main image to image Two
+let ChoiceTwo = document.getElementsByClassName('Choice-two')
+for (i = 0; i < ChoiceTwo.length; i++) {
+    let imageTwo = ChoiceTwo[i]
+    let imageTwoUrl = window.getComputedStyle(imageTwo).getPropertyValue('background-image')
+
+    imageTwo.addEventListener('click', () => {
+        mainPhoto.style.backgroundImage = imageTwoUrl
+    })
+}
+
+
+let sizeDiv = document.getElementById('size')
+let eachSizeDiv = sizeDiv.querySelectorAll('div')
+let SSizeDiv = document.getElementById('S')
+let previousDiv = null;
+Array.from(eachSizeDiv).forEach((div) => {
+    div.addEventListener('click', (e) => {
+        //to clear the previous div color
+        if (previousDiv) {
+            previousDiv.style.background = "white"
+        }
+
+        SSizeDiv.style.backgroundColor = "white"
+        div.style.background = "#89808046"
+        previousDiv = div
+    })
+})
