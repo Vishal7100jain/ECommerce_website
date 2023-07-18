ae541b5d96cc2ba186f526e0867748a4a54bc1c7
Vishal <vishaljain7100@gmail.com>
2023-07-18 23:01:39 +0530

    separated the files in diff folders and page is ready working on navbar and detials of page

--- Men's Wear/menswear.js
+++ Men's Wear/menswear.js
@@ -1,21 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
-  localStorage.clear()
-})
-let section = document.getElementById('section')
-let sectionDiv = section.querySelectorAll('div')
-Array.from(sectionDiv).forEach((div) => {
-  div.addEventListener('click', (event) => {
-    
-    var selectedDiv = event.target;
-    var DivBgImageSRC = window.getComputedStyle(selectedDiv).getPropertyValue('background-image');
-    localStorage.setItem('postRequestBody', DivBgImageSRC);
+let ShirtPage = document.getElementById('MensWear-page-Shirt')
+let JeansPage = document.getElementById('MensWear-page-Jeans')
+let WatchPage = document.getElementById('MensWear-page-Watch')
+let TrackPage = document.getElementById('MensWear-page-Tracks')
+let TShirtPage = document.getElementById('MensWear-page-Tshirts')
+let ShoesPage = document.getElementById('MensWear-page-Shoes')
 
-      for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        if (key.startsWith("postRequestBody")) {
-          window.open("/commonPage/Product.html", "_blank")
-        }
-      }
-  });
-});
+//functionlly to open selected product page
+ShirtPage.addEventListener('click', () => {
+  window.open("Shirt Page/Shirt.html", "_blank")
+})
+
+JeansPage.addEventListener('click', () => {
+  window.open("Jeans Page/Jeans.html", "_blank")
+})
+
+WatchPage.addEventListener('click', () => {
+  window.open("Watch page/Watch.html", "_blank")
+})
+
+TrackPage.addEventListener('click', () => {
+  window.open("Track Page/Track.html", "_blank")
+})
+
+TShirtPage.addEventListener('click', () => {
+  window.open("T Shirt Page/TShirt.html", "_blank")
+})
+
+ShoesPage.addEventListener('click', () => {
+  window.open("Shoes Page/Shoes.html", "_blank")
+})
+
 
--- commonPage/commonpage.js
+++ commonPage/commonpage.js
@@ -1,59 +1,92 @@
-let postRequestBody = localStorage.getItem('postRequestBody');
+let SelectedProduct = localStorage.getItem('SelectedProduct');
+let ShirtBackimage = "url('https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/s/g/p/l-st1-vebnor-original-imagmsyxunkgztzw.jpeg?q=70')"
+let jeansBackimage = "url('https://rukminim2.flixcart.com/image/832/832/xif0q/jean/c/v/e/-original-imaghseb6wjdg9un.jpeg?q=70')"
+let ShoesBackimage = "url('https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/j/v/9/6-tm-12-6-trm-white-original-imagjqyzntcvcmhz.jpeg?q=90')"
+let TracksBackimage = "url('https://dpassion.com/cdn/shop/products/057A5313_1000x.jpg?v=1678771278')"
+let TShirtBackimage = "url('https://rukminim2.flixcart.com/image/832/832/jv5k2a80/t-shirt/e/f/h/m-msp19pn010b-metronaut-original-imafg49sb7z5ecx7.jpeg?q=70')"
+let WatchBackimage = "url('https://rukminim2.flixcart.com/image/416/416/xif0q/smartwatch/d/u/k/-original-imagzzukgevbkh9w.jpeg?q=70')"
 
-if (postRequestBody) {
+if (SelectedProduct) {
     //Main image of the product
-    let mainPhoto = document.getElementById("mainPhotodiv")
-    mainPhoto.style.backgroundImage = postRequestBody;
+    var mainPhoto = document.getElementById("mainPhotodiv")
+    mainPhoto.style.backgroundImage = SelectedProduct;
 
-    //Choice for color
-    let ChoiceOne = document.getElementsByClassName('Choice-one');
+    //Choice image one
+    var ChoiceOne = document.getElementsByClassName('Choice-one');
     for (let i = 0; i < ChoiceOne.length; i++) {
-        let divElementone = ChoiceOne[i];
-        divElementone.style.backgroundImage = postRequestBody;
+        var imageOne = ChoiceOne[i];
+        imageOne.style.backgroundImage = SelectedProduct;
+        //Too change the main image to the seleted image
+        imageOne.addEventListener('click', () => {
+            mainPhoto.style.backgroundImage = SelectedProduct
+        })
     }
 
-    let ChoiceTwo = document.getElementsByClassName('Choice-two');
+    //Choice image two
+    var ChoiceTwo = document.getElementsByClassName('Choice-two');
     for (let i = 0; i < ChoiceTwo.length; i++) {
-        let divElementtwo = ChoiceTwo[i];
-        divElementtwo.style.backgroundImage = postRequestBody;
+        var imageTwo = ChoiceTwo[i];
+        imageTwo.style.backgroundImage = SelectedProduct;
+
+        //Too change the main image to the seleted image
+        function imagechangingTwo(imageURL) {
+            imageTwo.addEventListener('click', (e) => {
+                mainPhoto.style.backgroundImage = imageURL
+            })
+        }
 
         //setting the back image 
         //back image for Shirt
-        if (postRequestBody == `url("https://image.made-in-china.com/2f0j00FjRYyOfKkPke/Men-s-Shirt-Fashion-Cotton-Hanging-Dyed-Gradient-Button-Chic-Shirts.webp")`) {
-            let ShirtBackimage = "url('https://static-01.daraz.pk/p/9ca45c6a6ee3aa5ba34b10cde93a6e61.jpg')"
-            divElementtwo.style.backgroundImage = ShirtBackimage
+        if (SelectedProduct == `url("https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/s/u/f/s-st10-vebnor-original-imagprjgsdzafrqb.jpeg?q=70")`) {
+            imageTwo.style.backgroundImage = ShirtBackimage
+            imagechangingTwo(`${ShirtBackimage}`)
         }
+
         //back image for jeans
-        else if (postRequestBody == `url("https://rukminim2.flixcart.com/image/832/832/xif0q/jean/n/r/2/-original-imaghsebtyzqzkyv.jpeg?q=70")`) {
-            let jeansBackimage = "url('https://rukminim2.flixcart.com/image/832/832/xif0q/jean/c/v/e/-original-imaghseb6wjdg9un.jpeg?q=70')"
-            divElementtwo.style.backgroundImage = jeansBackimage
+        else if (SelectedProduct == `url("https://rukminim2.flixcart.com/image/832/832/xif0q/jean/n/r/2/-original-imaghsebtyzqzkyv.jpeg?q=70")`) {
+            imageTwo.style.backgroundImage = jeansBackimage
+            imagechangingTwo(`${jeansBackimage}`)
         }
 
         //back image for Shoes
-        else if (postRequestBody == `url("https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/7/2/m/6-tm-12-6-trm-white-original-imagjqyzz8z9jrgf.jpeg?q=90")`) {
-            let ShoesBackimage = "url('https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/j/v/9/6-tm-12-6-trm-white-original-imagjqyzntcvcmhz.jpeg?q=90')"
-            divElementtwo.style.backgroundImage = ShoesBackimage
+        else if (SelectedProduct == `url("https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/7/2/m/6-tm-12-6-trm-white-original-imagjqyzz8z9jrgf.jpeg?q=90")`) {
+            imageTwo.style.backgroundImage = ShoesBackimage
+            imagechangingTwo(`${ShoesBackimage}`)
         }
 
         //back image for Tracks
-        else if (postRequestBody == `url("https://dpassion.com/cdn/shop/products/057A5307_1000x.jpg?v=1678771278")`) {
-            let TracksBackimage = "url('https://dpassion.com/cdn/shop/products/057A5313_1000x.jpg?v=1678771278')"
-            divElementtwo.style.backgroundImage = TracksBackimage
+        else if (SelectedProduct == `url("https://dpassion.com/cdn/shop/products/057A5307_1000x.jpg?v=1678771278")`) {
+            imageTwo.style.backgroundImage = TracksBackimage
+            imagechangingTwo()
+            imagechangingTwo(`${TracksBackimage}`)
         }
 
         //back image for T-shirt
-        else if (postRequestBody == `url("https://rukminim2.flixcart.com/image/832/832/kflftzk0-0/t-shirt/y/3/i/l-msp19pn010b-metronaut-original-imafwyf78bvf4rsy.jpeg?q=70")`) {
-            let TShirtBackimage = "url('https://rukminim2.flixcart.com/image/832/832/jv5k2a80/t-shirt/e/f/h/m-msp19pn010b-metronaut-original-imafg49sb7z5ecx7.jpeg?q=70')"
-            divElementtwo.style.backgroundImage = TShirtBackimage
+        else if (SelectedProduct == `url("https://rukminim2.flixcart.com/image/832/832/kflftzk0-0/t-shirt/y/3/i/l-msp19pn010b-metronaut-original-imafwyf78bvf4rsy.jpeg?q=70")`) {
+            imageTwo.style.backgroundImage = TShirtBackimage
+            imagechangingTwo(`${TShirtBackimage}`)
         }
 
         //back image for watch
-        else if (postRequestBody == `url("https://rukminim2.flixcart.com/image/416/416/xif0q/smartwatch/i/2/2/-original-imagzvhswuuzay6x.jpeg?q=70")`) {
-            let WatchBackimage = "url('https://rukminim2.flixcart.com/image/416/416/xif0q/smartwatch/d/u/k/-original-imagzzukgevbkh9w.jpeg?q=70')"
-            divElementtwo.style.backgroundImage = WatchBackimage
+        else if (SelectedProduct == `url("https://rukminim2.flixcart.com/image/416/416/xif0q/smartwatch/i/2/2/-original-imagzvhswuuzay6x.jpeg?q=70")`) {
+            imageTwo.style.backgroundImage = WatchBackimage
+            imagechangingTwo(`${WatchBackimage}`)
         }
 
     }
 } else {
     console.log('Background image URL not found in local storage');
 }
+
+
+//Funtionally for nav bar
+let jeansNavBtn = document.getElementById("jeans-nav-btn")
+let watchsNavBtn = document.getElementById("Watch-nav-btn")
+let TracksNavBtn = document.getElementById("Tracks-nav-btn")
+let TShirtNavBtn = document.getElementById("TShirt-nav-btn")
+let ShoesNavBtn = document.getElementById("Shoes-nav-btn")
+
+//function to add page according to navbar li click
+jeansNavBtn.addEventListener('click', () => [
+    mainPhoto.style.backgroundImage = SelectedProduct
+])
