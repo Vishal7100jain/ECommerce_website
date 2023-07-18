ab51ee7ffce721b1f904e4ea9c827d116e57866d
Vishal <vishaljain7100@gmail.com>
2023-07-18 23:26:48 +0530

    given the product page html css and js files name properly and position

---
+++ Men's Wear/ProductPage.js
@@ -0,0 +1,21 @@
+let ShirtBackimage = "url('https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/s/g/p/l-st1-vebnor-original-imagmsyxunkgztzw.jpeg?q=70')"
+let jeansBackimage = "url('https://rukminim2.flixcart.com/image/832/832/xif0q/jean/c/v/e/-original-imaghseb6wjdg9un.jpeg?q=70')"
+let ShoesBackimage = "url('https://rukminim2.flixcart.com/image/850/1000/xif0q/shoe/j/v/9/6-tm-12-6-trm-white-original-imagjqyzntcvcmhz.jpeg?q=90')"
+let TracksBackimage = "url('https://dpassion.com/cdn/shop/products/057A5313_1000x.jpg?v=1678771278')"
+let TShirtBackimage = "url('https://rukminim2.flixcart.com/image/832/832/jv5k2a80/t-shirt/e/f/h/m-msp19pn010b-metronaut-original-imafg49sb7z5ecx7.jpeg?q=70')"
+let WatchBackimage = "url('https://rukminim2.flixcart.com/image/416/416/xif0q/smartwatch/d/u/k/-original-imagzzukgevbkh9w.jpeg?q=70')"
+
+// Selecting all image div to change image based on image clicked
+let mainPhoto = document.getElementById('mainPhotodiv')
+
+// Choice one image
+let ChoiceOne = document.getElementsByClassName('.Choice-one')
+for (i = 0; i < ChoiceOne.length; i++) {
+    let imageOne = ChoiceOne[i]
+    imageOne.addEventListener('click', () => {
+        let imageOneUrl = window.getComputedStyle(ChoiceOne).getPropertyValue("background-image")
+        console.log(imageOneUrl)
+    })
+}
+let ChoiceTwo = document.getElementsByClassName('.Choice-two')
+
--- Men's Wear/menswear.js
+++ Men's Wear/menswear.js
@@ -29,25 +29,4 @@ TShirtPage.addEventListener('click', () => {
 ShoesPage.addEventListener('click', () => {
   window.open("Shoes Page/Shoes.html", "_blank")
 })
-<<<<<<< HEAD
-let section = document.getElementById('section')
-let sectionDiv = section.querySelectorAll('div')
-Array.from(sectionDiv).forEach((div) => {
-  div.addEventListener('click', (event) => {
-    
-    var selectedDiv = event.target;
-    var DivBgImageSRC = window.getComputedStyle(selectedDiv).getPropertyValue('background-image');
-    localStorage.setItem('SelectedProduct', DivBgImageSRC);
-
-      for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        if (key.startsWith("SelectedProduct")) {
-          window.open("/commonPage/Product.html", "_blank")
-        }
-      }
-  });
-});
-=======
-
->>>>>>> Menswear
 
