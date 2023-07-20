4ca2c624ede1617c03d24d18cb346cfe43a5b9b0
Vishal <vishaljain7100@gmail.com>
2023-07-20 13:40:14 +0530

    Add the product files for womeswear

---
+++ Women's Wear/WomensWear.js
@@ -0,0 +1,18 @@
+let WomenWearPageDress = document.getElementById('WomenWear-page-Dress')
+let WomenWearPageJeansTop = document.getElementById('WomenWear-page-JeansTop')
+let WomenWearPageSadi = document.getElementById('WomenWear-page-Sadi')
+
+//functionlly to open selected product page
+WomenWearPageDress.addEventListener('click', () => {
+  window.open("Dresses/Dress.html", "_blank")
+})
+
+WomenWearPageJeansTop.addEventListener('click', () => {
+  window.open("Jeans Top/JeansTop.html", "_blank")
+})
+
+WomenWearPageSadi.addEventListener('click', () => {
+  window.open("Womens Sadi/Sadi.html", "_blank")
+})
+
+
